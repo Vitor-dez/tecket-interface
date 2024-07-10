968ac21aed14d9057019b3f1874d795076999f27
Vitor-dez <vvitorsilva37@gmail.com>
2024-07-09 22:31:51 -0300

    Foi mudado o tamanho do container

--- src/containers/Login/styles.js
+++ src/containers/Login/styles.js
@@ -7,7 +7,7 @@ import styled from "styled-components";
 export const Container = styled.div`
 
 width: 100%;
-height: 100vh;
+height: 100dvh;
 display: flex;
 justify-content: center;
 align-items: center;
